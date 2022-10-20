427b6a7a56e56ff751dafa5fea5ef08bcbd11742
Masaki Murakami <yvzbtbcug@icloud.com>
2022-10-20 20:43:28 +0900

    Add contents

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,27 @@
 class SessionsController < ApplicationController
-  def new
-
-  end
+  before_action :set_user, only: [:create]
+  def new; end
 
   def create
-
+    if @user.present? && @user.authenticate(params[:password])
+      session[:user] = @user
+      # TODO: 画面遷移
+    else
+      failure_messages = []
+      failure_messages.push('ユーザーIDを入力してください') if params[:display_id].blank?
+      failure_messages.push('パスワードを入力してください') if params[:password].blank?
+      flash.now[:failure] = failure_messages.presence || ['ユーザーIDまたはパスワードが正しくありません']
+      render :new
+    end
   end
 
   def destroy
 
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(display_id: params[:display_id])
   end
 end
